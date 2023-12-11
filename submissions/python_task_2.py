e7bf038bbc1efd3d5c6c0441c2ac49583891855c
Preeti Sharma <preeti.sh258@gmail.com>
2023-12-11 23:51:27 +0530

    Update python_task_2.py

--- submissions/python_task_2.py
+++ submissions/python_task_2.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
 
-def calculate_distance_matrix(input_df)->pd.DataFrame:
+def calculate_distance_matrix(df)->pd.DataFrame:
     """
     Calculate a distance matrix based on the dataframe, df.
 
     Args:
-        input_df (pandas.DataFrame)
+        df (pandas.DataFrame)
 
     Returns:
         pandas.DataFrame: Distance matrix
     """
     # Check if the required columns are present
     # Create an empty distance matrix DataFrame
-    tolls = sorted(set(input_df['id_start'].unique()) | set(input_df['id_end'].unique()))
+    tolls = sorted(set(df['id_start'].unique()) | set(df['id_end'].unique()))
     distance_matrix = pd.DataFrame(index=tolls, columns=tolls)
     distance_matrix = distance_matrix.fillna(0)
 
     # Populate the distance matrix with known distances
-    for _, row in input_df.iterrows():
+    for _, row in df.iterrows():
         start_toll, end_toll, distance = row['id_start'], row['id_end'], row['distance']
         distance_matrix.at[start_toll, end_toll] += distance
         distance_matrix.at[end_toll, start_toll] += distance  # Account for bidirectional distances
@@ -54,12 +54,12 @@ def unroll_distance_matrix(distance_matrix):
     return unrolled_distances
 
 
-def find_ids_within_ten_percentage_threshold(input_df, reference_id)->pd.DataFrame:
+def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame:
     """
     Find all IDs whose average distance lies within 10% of the average distance of the reference ID.
 
     Args:
-        input_df (pandas.DataFrame)
+        df (pandas.DataFrame)
         reference_id (int)
 
     Returns:
@@ -68,7 +68,7 @@ def find_ids_within_ten_percentage_threshold(input_df, reference_id)->pd.DataFra
     """
     # Check if the required columns are present
     # Filter rows where 'id_start' is equal to the reference value
-    reference_rows = input_df[input_df['id_start'] == reference_id]
+    reference_rows = df[df['id_start'] == reference_id]
 
     # Calculate the average distance for the reference value
     reference_avg_distance = reference_rows['distance'].mean()
@@ -78,28 +78,30 @@ def find_ids_within_ten_percentage_threshold(input_df, reference_id)->pd.DataFra
     upper_threshold = reference_avg_distance + (0.1 * reference_avg_distance)
 
     # Filter 'id_start' values within the 10% threshold
-    within_threshold_ids = input_df[
-        (input_df['distance'] >= lower_threshold) &
-        (input_df['distance'] <= upper_threshold)]['id_start'].unique()
+    within_threshold_ids = df[
+        (df['distance'] >= lower_threshold) &
+        (df['distance'] <= upper_threshold)]['id_start'].unique()
     # Sort and return the list of 'id_start' values
     within_threshold_ids.sort()
     return within_threshold_ids
 
 
-def calculate_toll_rate(input_df)->pd.DataFrame:
+def calculate_toll_rate(df)->pd.DataFrame:
     """
     Calculate toll rates for each vehicle type based on the unrolled DataFrame.
 
     Args:
-        input_df (pandas.DataFrame)
+        df (pandas.DataFrame)
 
     Returns:
         pandas.DataFrame
     """
     # Calculate toll rates based on vehicle types and add columns to the DataFrame
-    input_df['moto'] = 0.8 * input_df['distance'] 
-    input_df['car'] = 1.2 * input_df['distance']
-    input_df['rv'] = 1.5 * input_df['distance']
-    input_df['bus'] = 2.2 * input_df['distance']
-    input_df['truck'] = 3.6 * input_df['distance']
-    return input_df
+    df['moto'] = 0.8 * df['distance'] 
+    df['car'] = 1.2 * df['distance']
+    df['rv'] = 1.5 * df['distance']
+    df['bus'] = 2.2 * df['distance']
+    df['truck'] = 3.6 * df['distance']
+    df.drop('distance', axis=1,inplace=True)
+    
+    return df
